eeb0bd28d21be8e0deed6222a6df2974423d4995
SinqaX <133186997+SinqaX@users.noreply.github.com>
2024-05-02 20:37:04 -0500

    ejercicios

--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,212 @@
-from email_validator import validate_email, EmailNotValidError
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+import sys
+import os
 
-def validar_correo_electronico(correo):
-    try:
-        # Usamos la función validate_email para validar el correo electrónico
-        validador = validate_email(correo)
-        # Si la dirección es válida, devuelve True
-        return True
-    except EmailNotValidError:
-        # Si ocurre un error, significa que la dirección no es válida, entonces devuelve False
-        return False
+from PySide6.QtWidgets import QWidget
 
-# Ejemplo de uso
-correo = "usuario@gmail.com"
-if validar_correo_electronico(correo):
-    print("El correo electrónico es válido.")
-else:
-    print("El correo electrónico no es válido.")
+# class mainWindow(QWidget):
+
+#     def __init__(self):
+#         super().__init__()
+
+#         self.ventanas = QStackedLayout()
+
+#         self.setWindowTitle("tres en raya")
+#         self.setMinimumSize(450,600)
+
+#         self.basedir = os.path.dirname(__file__)
+
+#         self.primera()
+#         self.segunda()
+#         self.tercera()
+
+
+#         self.setLayout(self.ventanas)
+
+
+
+#     def primera(self):
+
+#         widget = QWidget()
+#         qvbox = QVBoxLayout()
+
+#         label_img = QLabel()
+#         img = QPixmap(os.path.join(self.basedir, "img", "3enraya", "tic_tac_toe.png"))
+#         label_img.setPixmap(img.scaled(120,120))
+#         label_img.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+#         label = QLabel("Tres En Raya")
+#         label.setFont(QFont("Courier", 19))
+#         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+#         qform = QFormLayout()
+#         self.player_x = QLineEdit()
+#         self.player_o = QLineEdit()
+#         qform.addRow("Jugador X", self.player_x)
+#         qform.addRow("Jugador O", self.player_o)
+
+
+#         button = QPushButton("Jugar", self)
+#         button.clicked.connect(lambda: self.ventanas.setCurrentIndex(1))
+
+
+#         qvbox.addWidget(label_img)
+#         qvbox.addWidget(label)
+#         qvbox.addLayout(qform) 
+#         qvbox.addSpacing(120)
+#         qvbox.addWidget(button)
+
+
+#         widget.setLayout(qvbox)
+#         self.ventanas.addWidget(widget)
+
+
+
+#     def segunda(self):
+#         widget = QWidget()
+#         qvbox = QVBoxLayout()
+
+
+#         label = QLabel("Tres En Raya")
+#         label.setFont(QFont("Courier", 15))
+#         label.font().setBold(True)
+#         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+#         self.label_dinamico = QLabel("Turno de ", self)
+#         self.label_dinamico.setFont(QFont("Courier", 15))
+#         self.label_dinamico.font().setBold(True)
+#         self.label_dinamico.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+
+#         qgridbox = QGridLayout()
+#         for i in range(3):
+#             for j in range(3):
+#                 button = QPushButton()
+#                 button.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+#                 button.setFixedSize(130,130)
+#                 qgridbox.addWidget(button, i, j)
+
+        
+#         button = QPushButton()
+#         icon = QIcon(os.path.join(self.basedir, "img", "3enraya", "back_game.png"))
+#         button.setIcon(icon)
+#         button.setFixedSize(19,15)
+#         button.clicked.connect(lambda: self.ventanas.setCurrentIndex(2))
+
+
+#         qvbox.addWidget(label)
+#         qvbox.addWidget(self.label_dinamico)
+#         qvbox.addLayout(qgridbox)
+#         qvbox.addWidget(button, alignment=Qt.AlignmentFlag.AlignRight)
+
+
+
+#         widget.setLayout(qvbox)
+#         self.ventanas.addWidget(widget)
+
+#     def tercera(self):
+        
+#         widget = QWidget()
+#         qvbox = QVBoxLayout()
+
+#         label_img = QLabel()
+#         img = QPixmap(os.path.join(self.basedir, "img", "3enraya", "tic_tac_toe.png"))
+#         label_img.setPixmap(img.scaled(110,110))
+#         label_img.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+#         label = QLabel("By\n Sebastian David Ordoñez Bolaños")
+#         label.setFont(QFont("Courier", 15))
+#         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+#         button = QPushButton()
+#         icon = QIcon(os.path.join(self.basedir, "img", "3enraya", "about.png"))
+#         button.setIcon(icon)
+#         button.setFixedSize(19,15)
+
+#         qvbox.addSpacing(100)
+#         qvbox.addWidget(label_img)
+#         qvbox.setSpacing(0)
+#         qvbox.addWidget(label)
+#         qvbox.addSpacing(180)
+#         qvbox.addWidget(button, alignment=Qt.AlignmentFlag.AlignRight)
+#         button.clicked.connect(lambda: self.ventanas.setCurrentIndex(1))
+
+#         widget.setLayout(qvbox)
+#         self.ventanas.addWidget(widget)
+
+
+
+# if __name__=="__main__":
+#     app = QApplication([])
+#     window = mainWindow()
+#     window.show()
+#     sys.exit(app.exec())
+
+class mainWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        colores = ["green", "blue", "yellow", "pink", "gray", "orange", "purple"]
+
+        self.setWindowTitle("Colores")
+        self.setMinimumSize(30
+                             0,180)
+
+        principal = QHBoxLayout()
+
+        qvbox1 = QVBoxLayout()
+        qvbox2 = QVBoxLayout()
+        label = QLabel("4")
+        label.setFont(QFont("Courier", 60))
+        label.font().setBold(True)
+        label.setAutoFillBackground(True)
+        pallete = label.palette()
+        pallete.setColor(QPalette.Window, colores[6])
+        label.setPalette(pallete)
+        qvbox2.addWidget(label)
+        qvbox3 = QVBoxLayout()
+        
+        for i in range(6):
+            if i<3:
+                label = QLabel(f"{i+1}")
+                label.setFont(QFont("Courier", 60))
+                label.font().setBold(True)
+                label.setAutoFillBackground(True)
+                pallete = label.palette()
+                pallete.setColor(QPalette.Window, colores[i])
+                label.setPalette(pallete)
+                qvbox1.addWidget(label)
+            else:
+                if i == 4:
+                    qvbox3.addSpacing(40)
+                else:
+                    qvbox3.setSpacing(0)
+
+                label = QLabel(f"{i+1}")
+                label.setFont(QFont("Courier", 60))
+                label.font().setBold(True)
+                label.setAutoFillBackground(True)
+                pallete = label.palette()
+                pallete.setColor(QPalette.Window, colores[i])
+                label.setPalette(pallete)
+                
+                qvbox3.addWidget(label)
+
+        principal.addLayout(qvbox1)
+        principal.addLayout(qvbox2)
+        principal.addLayout(qvbox3)
+        
+        self.setLayout(principal)
+
+if __name__=="__main__":
+    app = QApplication([])
+    window = mainWindow()
+    window.show()
+    sys.exit(app.exec())
